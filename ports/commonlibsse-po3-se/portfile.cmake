ede3053e641cc9fb5b11bb618dc46884bb67c61e
Arc Earth <ArcEarth@outlook.com>
2024-03-23 00:00:56 -0700

    check sha512

--- ports/commonlibsse-po3-se/portfile.cmake
+++ ports/commonlibsse-po3-se/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO powerof3/CommonLibSSE
         REF 919033346b867bedf25cb31660c8c2990872b593
-        SHA512 d23cd381e81d3207fb63267190e94abb6e6bfe1f836e60476c0080d3154249755a869a35d8a5c4939beac9f6511fa9ad840a3c19e42385b3c1d9f869f0ce11e1
+        SHA512 0
         HEAD_REF dev
 )
 
