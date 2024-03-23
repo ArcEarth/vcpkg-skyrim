4ee43f9c3946fd4b8f6044e1ba38bf5f0fce8bf1
Arc Earth <ArcEarth@outlook.com>
2024-03-23 00:10:41 -0700

    Update sha512

--- ports/commonlibsse-po3-se/portfile.cmake
+++ ports/commonlibsse-po3-se/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO powerof3/CommonLibSSE
         REF 919033346b867bedf25cb31660c8c2990872b593
-        SHA512 0
+        SHA512 e44cdaa601fddb7b979f1b094b633c731e31ea31d4a164976be947001ccc81f5e853d13d4c2cb73cae1dca8db56bd96269020cd27f7dbd3d38958d985c1922b9
         HEAD_REF dev
 )
 
